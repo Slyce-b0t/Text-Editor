7892e8557f957ebc6a15e51685f02a3b5ce4d3cd
Tural <89679822+Slyce-b0t@users.noreply.github.com>
2022-05-26 20:41:24 +0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -3,7 +3,7 @@ from tkinter import *
 from functions import *
 
 root = Tk()
-root.title('My cool text editor')
+root.title('binary text editor')
 root.geometry("900x700")
 
 # Create main frame
@@ -19,24 +19,24 @@ hor_scroll = Scrollbar(frame, orient='horizontal')
 hor_scroll.pack(side=BOTTOM, fill=X)
 
 # Create text box
-my_text = Text(frame, width=75, height=25, font=("Minion Pro", 16), selectbackground="blue",
+binary_text = Text(frame, width=75, height=25, font=("Minion Pro", 16), selectbackground="blue",
                selectforeground="black", undo=True, yscrollcommand=ver_scroll.set, wrap="none",
                xscrollcommand=hor_scroll.set)
-my_text.pack()
+binary_text.pack()
 
-# Configure our scrollbar
-ver_scroll.config(command=my_text.yview)
-hor_scroll.config(command=my_text.xview)
+# Configure our scrollbars
+ver_scroll.config(command=binary_text.yview)
+hor_scroll.config(command=binary_text.xview)
 
 # Create menu
-my_menu = Menu(root)
-root.config(menu=my_menu)
+binary_menu = Menu(root)
+root.config(menu=binary_menu)
 
 # Add edit menu
-edit_menu = Menu(my_menu, tearoff=False)
-my_menu.add_cascade(label="Edit", menu=edit_menu)
-edit_menu.add_command(label="Cut", command=lambda: cut_text(False, my_text, root), accelerator="(Ctrl+x)")
-edit_menu.add_command(label="Copy", command=lambda: copy_text(False, my_text, root), accelerator="(Ctrl+c)")
-edit_menu.add_command(label="Paste", command=lambda: paste_text(False, my_text, root), accelerator="(Ctrl+v)")
+edit_menu = Menu(binary_menu, tearoff=False)
+binary_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=lambda: cut_text(binary_text, root))
+edit_menu.add_command(label="Copy", command=lambda: copy_text(binary_text, root))
+edit_menu.add_command(label="Paste", command=lambda: paste_text(binary_text))
 
 root.mainloop()
